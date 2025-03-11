1a206d8f991d1d52d3d12d7e87d5ed859fd31d27
Jay8303 <107171637+Jay8303@users.noreply.github.com>
2025-03-11 18:18:35 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+echo "Fixing permissions..."
+sudo chown -R ec2-user:ec2-user /home/ec2-user/my-app
+sudo chmod -R 755 /home/ec2-user/my-app
+
 echo "Installing production dependencies..."
 cd /home/ec2-user/my-app
 npm install --production
