3d4f06408c5bca09700102421d96b8f4c80fb222
Jay8303 <107171637+Jay8303@users.noreply.github.com>
2025-03-11 18:05:31 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-echo "Cleaning old application files..."
+
+# Make sure ec2-user owns all files
+sudo chown -R ec2-user:ec2-user /home/ec2-user/my-app
+
+# Clean old files
 rm -rf /home/ec2-user/my-app/*
+
+# Optionally, recreate folders if needed
+mkdir -p /home/ec2-user/my-app/scripts
